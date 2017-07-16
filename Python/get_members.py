b6e8fd15eb63cdfe2e6e5e8388bec74d2a3d26d9
proincm <proincm@gmail.com>
2017-07-17 04:45:09 +0800

    Create get_members.py

---
+++ Python/get_members.py
@@ -0,0 +1,17 @@
+import re
+
+
+
+path='15/2+3'
+print(re.sub(',(?!S)',' , ',path))
+
+class xxx(object):
+  position = 22
+  def sony(self):
+    return 15
+  def pupi(self):
+    return
+
+
+ist = [func for func in dir(re) if callable(getattr(re, func)) and not func.startswith("__")]
+print(ist)
