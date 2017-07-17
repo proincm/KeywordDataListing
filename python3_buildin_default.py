65e0f3a69db1ca3058188d2a7455289a507d5c12
proincm <proincm@gmail.com>
2017-07-17 13:47:36 +0800

    Create python3_buildin_default.py

---
+++ python3_buildin_default.py
@@ -0,0 +1,11 @@
+__import__ | abs | all | any | ascii | basestring | bin | bool |
+          bytearray | bytes | callable | chr | classmethod | cmp | compile |
+          complex | delattr | dict | dir | divmod | enumerate | eval | exec |
+          execfile | file | filter | float | format | frozenset | getattr |
+          globals | hasattr | hash | help | hex | id | input | int |
+          isinstance | issubclass | iter | len | list | locals | long | map |
+          max | memoryview | min | next | object | oct | open | ord | pow |
+          property | range | raw_input | reduce | reload | repr |
+          reversed | round | set | setattr | slice | sorted | staticmethod |
+          str | sum | super | tuple | type | unichr | unicode | vars |
+          xrange | zip
